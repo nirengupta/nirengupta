7b57a6645b0ca6f49ff98ea5397697d760df8e5b
nirengupta <nireng553@gmail.com>
2022-09-22 17:45:56 +0530

    2ndcommit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SautinSoft;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathToPdf = @"D:\SETFLSEProg\SETFLSEProg.pdf";
+            string pathToExcel = Path.ChangeExtension(pathToPdf, ".xls");
+            
+            SautinSoft.PdfFocus f = new SautinSoft.PdfFocus();
+
+   
+            f.ExcelOptions.ConvertNonTabularDataToSpreadsheet = false;
+
+            // 'true'  = Preserve original page layout.
+            // 'false' = Place tables before text.
+            f.ExcelOptions.PreservePageLayout = true;
+
+            // The information includes the names for the culture, the writing system,
+            // the calendar used, the sort order of strings, and formatting for dates and numbers.
+            System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("en-US");
+            ci.NumberFormat.NumberDecimalSeparator = ",";
+            ci.NumberFormat.NumberGroupSeparator = ".";
+            f.ExcelOptions.CultureInfo = ci;
+
+            f.OpenPdf(pathToPdf);
+
+            if (f.PageCount > 0)
+            {
+                int result = f.ToExcel(pathToExcel);
+
+                // Open the resulted Excel workbook.
+                if (result == 0)
+                {
+                    System.Diagnostics.Process.Start(pathToExcel);
+                }
+            }
+        }
+    }
+}
